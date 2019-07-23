0ec06a8be862a8683dd9e7f32081a40943fc7bc4
Adam Tse <mr.adam.tse@gmail.com>
2019-07-24 01:50:58 +0800

    add buttons and questions by raisedButton

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,32 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    void anwserQuestion() {
+      print('clicked 1');
+    }
+    var questions = [
+      'What\'s your favorite colour?',
+      'What\'s your favorite animal?',
+    ];
     return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text("Hello World")),
-      body: Text('This is the body'),
+      appBar: AppBar(
+          title: Text("Hello World")),
+      body: Column(
+        children: [
+          Text('Questions'),
+          RaisedButton(
+              child: Text('anwser 1'),
+              onPressed: anwserQuestion
+          ),
+          RaisedButton(
+              child: Text('anwser 2'),
+              onPressed: () => print('clicked 2')
+          ),
+          RaisedButton(
+              child: Text('anwser 3'),
+              onPressed: () => print('clicked 3')
+          ),
+        ]),
     ));
   }
 }
